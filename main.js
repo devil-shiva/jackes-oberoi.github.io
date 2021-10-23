4ba0bd30b5a867893833446d73d2cecfa6f2a8f9
devil-shiva <shivamsaksham2000@gmail.com>
2021-10-23 10:58:13 +0530

    Final

--- main.js
+++ main.js
@@ -22,3 +22,20 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.materialboxed');
     var instances = M.Materialbox.init(elems);
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+var elems = document.querySelectorAll('.carousel');
+var instances = M.Carousel.init(elems , {
+    duration:50
+});
+});
+
+
+let msgSendBtn = document.getElementById('sendMsg')
+let userEmail = "rishurishav3@gmail.com"
+let fname = document.getElementById('first_name')
+let lname = document.getElementById('last_name')
+let msg = document.getElementById('icon_prefix2')
+msgSendBtn.addEventListener('click' , ()=>{
+    window.location.href = `mailto:${userEmail}?subject=From%20${fname.value}%20${lname.value}&body=${msg.value}`
+})
